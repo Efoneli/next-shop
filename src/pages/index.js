bee16e3e96334a9f455c3cabb6e0bd783672a0d1
efoneli <abbaefoneli@gmail.com>
2023-05-22 16:10:24 +0100

    strapi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import Head from 'next/head'
 import Title from '../../components/Title';
 
+const products = [
+  { id: 1, title: "First Product" },
+  { id: 2, title: "Second Product" },
+]
+
 function HomePage() {
+  console.log('[HomePage] render:', products);
   return (
     <>
       <Head>
@@ -9,9 +15,13 @@ function HomePage() {
       </Head>
       <main className='px-6 py-4'>
         <Title>Next Shop</Title>
-        <p>
-          [TODO: display products]
-        </p>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.title}
+            </li>
+          ))}
+        </ul>
       </main>
     </>
   );
