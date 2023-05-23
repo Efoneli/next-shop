3afc427868e8e91bd54b72b680c26f8fd90ee36f
efoneli <abbaefoneli@gmail.com>
2023-05-23 09:20:39 +0100

    today

---
+++ src/pages/index-1.js
@@ -0,0 +1,38 @@
+//Option 1: fetch products on the server side (getStaticProps)
+
+import Head from 'next/head'
+import Title from '../../components/Title';
+
+// const products = [
+//   { id: 1, title: "First Product" },
+//   { id: 2, title: "Second Product" },
+// ]
+
+export async function getStaticProps() {
+    const response = await fetch('https://localhost:1337/products');
+    const products = await response.json();
+}
+
+function HomePage() {
+
+    
+  console.log('[HomePage] render:', products);
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main className='px-6 py-4'>
+        <Title>Next Shop</Title>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+}
+export default HomePage;
