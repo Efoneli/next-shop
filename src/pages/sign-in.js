ec31aa9b79e90ad2a080de7f1422e65541cb67a0
efoneli <abbaefoneli@gmail.com>
2023-06-01 10:53:44 +0100

    login

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -3,36 +3,65 @@ import Button from "../../components/Button";
 import Page from "../../components/Page";
 import Field from "../../components/field";
 import Input from "../../components/input";
+import { fetchJson } from "../../lib/api";
+
+
 
 function SignInPage() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [status, setStatus] = useState({ loading: false, error: false });
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log('should submit', {email, password})
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setStatus({ loading: true, error: false });
+    try {
+      const response = await fetchJson("http://localhost:1337/auth/local", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ identifier: email, password }),
+      });
+      setStatus({ loading: false, error: false });
+      console.log("sign in", response);
+    } catch (err) {
+      setStatus({ loading: false, error: true });
     }
+  };
 
-    return (
-        <Page title='Sign In' >
-            <form onSubmit={handleSubmit}>
-                <Field label='Email'>
-                    <Input type='email' required value={email}
-                    onChange={(event) => setEmail(event.target.value)} 
-                    />
-                </Field>
-                <Field label='Password'>
-                    <Input type='password' required value={password} 
-                    onChange={(event) => setPassword(event.target.value)} 
-                    />
-                </Field>
-            </form>
-            <Button type='submit'>
-                Sign In
+  return (
+    <Page title="Sign In">
+      <form onSubmit={handleSubmit}>
+        <Field label="Email">
+          <Input
+            type="email"
+            required
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </Field>
+        <Field label="Password">
+          <Input
+            type="password"
+            required
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </Field>
+        {status.error && (
+        <p className="text-red-700">
+            Invalid Credentials
+            </p>
+            )}
+        {status.loading ? (
+          <p>Loading...</p>
+        ) : (
+          <Button type="submit">
+            Sign In
             </Button>
-
-        </Page>
-    );
+        )}
+      </form>
+    </Page>
+  );
 }
 
 export default SignInPage;
